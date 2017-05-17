57bb9ba48a053c5d5a74c06fb01e22ead4c75bf1
Josse Coen <josse.coen@gmail.com>
2017-05-17 23:41:23 +0200

    Actually works now (still computationally unfeasible it seems)

--- minesweeper2.pl
+++ minesweeper2.pl
@@ -9,28 +9,70 @@ allZeroRow([]).
 allZeroRow([0 | Tail]) :- allZeroRow(Tail).
 allZeroRow([* | Tail]) :- allZeroRow(Tail).
 
+minus_one(8, 7).
+minus_one(7, 6).
+minus_one(6, 5).
+minus_one(5, 4).
+minus_one(4, 3).
+minus_one(3, 2).
+minus_one(2, 1).
+minus_one(1, 0).
+minus_one(0, -1).
+minus_one(-1, -2).
+minus_one(-2, -3).
+minus_one(-3, -4).
+minus_one(-4, -5).
+minus_one(-5, -6).
+minus_one(-6, -7).
+minus_one(-7, -8).
+
+valid_number(8).
+valid_number(7).
+valid_number(6).
+valid_number(5).
+valid_number(4).
+valid_number(3).
+valid_number(2).
+valid_number(1).
+valid_number(0).
+valid_number(-1).
+valid_number(-2).
+valid_number(-3).
+valid_number(-4).
+valid_number(-5).
+valid_number(-6).
+valid_number(-7).
+valid_number(-8).
+
 % first list is the row we are subtracting the numbers from; second list is
 % the one we are referencing in order to adapt the first;
 % third is the adapted list.
 
+reduce(*, *, *).
+reduce(Whatever, Number, Whatever) :- valid_number(Number).
+reduce(Number, *, NewNumber) :- minus_one(Number, NewNumber).
+
 reduceNumbersRow([], [_|_], []).
 reduceNumbersRow(Remainder, [], Remainder).
 
+reduceNumbersRow([Subtract | SubtractingTail], [Reference | ReferenceTail], [Reduced | ReducedTail]) :-
+    reduce(Subtract, Reference, Reduced),
+    reduceNumbersRow(SubtractingTail, ReferenceTail, ReducedTail).
+
 % There is a mine; account for it in the reference list.
-reduceNumbersRow([Number | SubtractingTail], [* | ReferenceTail], [NewNumber | Adapted]) :-
-    number(Number),
-    Number =< 8,
-    Number >= 0,
-    reduceNumbersRow(SubtractingTail, ReferenceTail, Adapted),
-    NewNumber is Number - 1.
+%% reduceNumbersRow([Number | SubtractingTail], [* | ReferenceTail], [NewNumber | Adapted]) :-
+%%     minus_one(Number, NewNumber),
+%%     reduceNumbersRow(SubtractingTail, ReferenceTail, Adapted).
 
-reduceNumbersRow([* | SubtractingTail], [_ | ReferenceTail], [* | Adapted]) :-
-    reduceNumbersRow(SubtractingTail, ReferenceTail, Adapted).
 
-% There is no mine; do not change subtracting list.
-reduceNumbersRow([Unchanged | SubtractingTail], [Number | ReferenceTail], [Unchanged | Adapted]) :-
-    not(mine(Number)),
-    reduceNumbersRow(SubtractingTail, ReferenceTail, Adapted).
+%% reduceNumbersRow([* | SubtractingTail], [_ | ReferenceTail], [* | Adapted]) :-
+%%     reduceNumbersRow(SubtractingTail, ReferenceTail, Adapted).
+
+%% % There is no mine; do not change subtracting list.
+%% reduceNumbersRow([Unchanged | SubtractingTail], [Number | ReferenceTail], [Unchanged | Adapted]) :-
+%%     valid_number(Unchanged),
+%%     valid_number(Number),
+%%     reduceNumbersRow(SubtractingTail, ReferenceTail, Adapted).
 
 % Initiate
 reduceNumbersGrid([FirstRow, SecondRow | Tail], Reduced) :-
@@ -91,4 +133,15 @@ validBoard(Board) :- reduceNumbersGrid(Board, Reduced), allZeroRows(Reduced).
 
 printReduce(Board) :- reduceNumbersGrid(Board, Reduced), write(Reduced).
 
-minesweeper(Query, Result) :- validBoard(Query), write(Query).
+printBoard([]).
+printBoard([Row | Rows]) :- printRow(Row), printBoard(Rows).
+
+printRow([]) :- nl.
+printRow([Tile | Tiles]) :- write(Tile), write(" "), printRow(Tiles).
+
+minesweeper(Query) :- validBoard(Query), printBoard(Query).
+
+board(3, [
+    [_, 2],
+    [2, _]
+]).
